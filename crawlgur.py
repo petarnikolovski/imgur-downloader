ac0c952a56869c054cac29b461f7e2dd94c7893a
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-06 16:50:08 +0200

    Add argument parser

---
+++ crawlgur.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+TODO: Add >how to<
+"""
+
+
+from argparse import ArgumentParser
+
+
+def parse_arguments():
+    """
+    Parse input arguments of the program.
+    """
+    parser = ArgumentParser(description='Imgur downloader')
+
+    parser.add_argument('-v', '--verbose',
+                        help='display download progress and information',
+                        action='store_true')
+    parser.add_argument('URL', help='source link')
+    parser.add_argument('directory', help='destination directory')
+
+    args = parser.parse_args()
+
+    return (args.verbose, args.URL, args.directory)
+
+
+if __name__ == '__main__':
+    verbose, url, destination = parse_arguments()
+
+    print(verbose, url, destination)
