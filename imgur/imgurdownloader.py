358aa1e3fd65ee8d9905381a7bf54edfbfe31419
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-07 00:52:22 +0200

    Upgrade sanitize pattern

--- imgur/imgurdownloader.py
+++ imgur/imgurdownloader.py
@@ -32,7 +32,7 @@ class ImgurDownloader(object):
         """
         Check if the supplied link is valid. If not, raise ImgurException.
         """
-        if re.match('https*\:\/\/(i.)*imgur\.com', url):
+        if re.match('https*\:\/\/(i.)*imgur\.com\/', url):
             return url
         raise ImgurException('Invalid link.')
 
