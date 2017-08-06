5588a8de348679cad941316226e4175154c25e7b
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-07 00:01:42 +0200

    Add fixutres

--- tests/test_imgurdownloader.py
+++ tests/test_imgurdownloader.py
@@ -11,6 +11,26 @@ class TestImgurDownloader(object):
     Class that tests imgur downloader class from the main package.
     """
 
+    @pytest.fixture
+    def single_image_i():
+        return ImgurDownloader('http://i.imgur.com/0s7mWKz.jpg')
+
+    @pytest.fixture
+    def single_image_without_extension_i():
+        return ImgurDownloader('http://i.imgur.com/0s7mWKz')
+
+    @pytest.fixture
+    def single_image_without_extension():
+        return ImgurDownloader('http://imgur.com/0s7mWKz')
+
+    @pytest.fixutre
+    def album_a():
+        return ImgurDownloader('http://imgur.com/a/vTTHZ')
+
+    @pytest.fixture
+    def album_gallery():
+        return ImgurDownloader('http://imgur.com/gallery/vTTHZ')
+
     def test_invalid_link(self):
         """
         Test if the class raises exception for invalid link.
