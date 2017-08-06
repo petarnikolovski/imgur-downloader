2a4915ab670ef3d1830005845bc37677544e7940
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-08-06 17:03:41 +0200

    Add barebones

--- imgur/imgurdownloader.py
+++ imgur/imgurdownloader.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+
+"""
+TODO: >add docstring<
+"""
+
+
+class ImgurDownloader(object):
+    """
+    ImgurDownloader contains all necessary methods to extract image or
+    album images from imgur link.
+    """
+
+    def __init__(self, url):
+        """
+        Initiate ImgurDownloader object.
+        """
+        pass
